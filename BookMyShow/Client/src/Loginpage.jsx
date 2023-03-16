6c8b1c4b4797566cae95384ee8ab239a54a87fbd
SHAJAN29 <m.j.sandy2916@gmail.com>
2023-03-16 17:54:05 +0530

    Login page completed

--- BookMyShow/Client/src/Loginpage.jsx
+++ BookMyShow/Client/src/Loginpage.jsx
@@ -1,10 +1,33 @@
 import { Button, TextField } from "@mui/material";
 import { useFormik } from "formik";
+
+import { useNavigate } from "react-router-dom";
 export function Loginpage() {
+  const navigate = useNavigate();
+
   const { values, handleChange, handleSubmit } = useFormik({
-    initialValues: { UserName: "20", Password: "20" },
-    onSubmit: (value) => {
+    initialValues: { username: "20", password: "20" },
+    onSubmit: async (value) => {
       console.log(value);
+      const data = await fetch(
+        "http://localhost:4000/bookingmovie/user-login",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(value),
+        }
+      );
+      console.log(value);
+      const result = await data.json();
+      if (data.status === 401) {
+        console.log("errorrrrr");
+      } else {
+        console.log(result);
+        localStorage.setItem("token", result.token);
+        navigate("/Movies");
+      }
     },
   });
 
@@ -16,18 +39,18 @@ export function Loginpage() {
         <TextField
           id="outlined-basic"
           label="UserName"
-          value={values.UserName}
+          value={values.username}
           variant="outlined"
           onChange={handleChange}
-          name="UserName"
+          name="username"
         />
 
         <TextField
           id="outlined-basic"
           type="password"
-          value={values.Password}
+          value={values.password}
           onChange={handleChange}
-          name="Password"
+          name="password"
           label="Password"
           variant="outlined"
         />
--- BookMyShow/Client/src/MovieLists.jsx
+++ BookMyShow/Client/src/MovieLists.jsx
@@ -1,6 +1,7 @@
 import { MovieCard } from "./MovieCard";
 
 export function MovieLists({ mv }) {
+  console.log(mv);
   return (
     <div className="MovieLists">
       {mv.map((ele, index) => (
